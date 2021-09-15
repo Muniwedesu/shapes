8d806a34a78ebf98d867c883d03ca7cf77bef4e5
Muniwedesu <mfmffmm@gmail.com>
2021-09-15 17:59:00 +0300

    added query & readme

---
+++ query.sql
@@ -0,0 +1,5 @@
+﻿-- Никогда не использовал T-SQL, но запрос написал бы так.
+
+SELECT product.product_id, category_id 
+FROM product LEFT JOIN product_category
+    ON product.product_id = product_category.product_id;
